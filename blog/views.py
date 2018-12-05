465610d952e666de23a2404929dc0a856297ed55
Evan Hwang <evan.h@kakaocorp.com>
2018-12-05 18:02:17 +0900

    Detail View 설정

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -10,4 +10,15 @@ def post_list(request):
     qs = qs.order_by('published_date')
     return render(request, 'blog/post_list.html', {
         'post_list': qs
+    })
+
+def post_detail(request, pk):
+    # try:
+    #     post = Post.objects.get(pk=pk);
+    # except Post.DoesNotExist:
+    #     raise Http404
+    post = get_object_or_404(Post, pk=pk)
+
+    return render(request, 'blog/post_detail.html', {
+        'post': post
     })
