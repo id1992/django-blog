2269e7ff04a66d6b8df4b9b0189849f80e1c4f1e
Evan Hwang <evan.h@kakaocorp.com>
2018-12-05 17:05:28 +0900

    django template css 적용

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # render는 파이썬 단에서 지원해주는 시스템
 # 리스트 명명법 - model name + _list
 def post_list(request):
-    return render(request, 'blog/post_list.html')
+    qs = Post.objects.all()
+    qs = qs.filter(published_date__lte=timezone.now())
+    qs = qs.order_by('published_date')
+    return render(request, 'blog/post_list.html', {
+        'post_list': qs
+    })
