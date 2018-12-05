fecf860db842ac63caa1e57b8f48e3accedd7770
Evan Hwang <evan.h@kakaocorp.com>
2018-12-05 19:11:45 +0900

    Post 생성 기능 추가

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # render는 파이썬 단에서 지원해주는 시스템
 # 리스트 명명법 - model name + _list
@@ -21,4 +22,20 @@ def post_detail(request, pk):
 
     return render(request, 'blog/post_detail.html', {
         'post': post
+    })
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/post_edit.html', {
+        'form': form
     })
